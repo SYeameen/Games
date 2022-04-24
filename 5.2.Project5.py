43cdb1a7ce6d0d4f845a12fbb32dc36f141986be
SYeameen <67371564+SYeameen@users.noreply.github.com>
2022-04-25 04:40:53 +0600

    first commit

---
+++ 5.2.Project5.py
@@ -0,0 +1,63 @@
+# import random
+# num_A = random.randint(1,10)
+# print("Your First num: ",num_A)
+# num_B = random.randint(1,10)
+# print("Your First num: ",num_B)
+# result=num_A*num_B
+# guessTheProduct=int(input("Guess the product of both numbers: "))
+
+# if result==guessTheProduct:
+#     print("Congratulations!!! You are a winner..")
+# else:
+#     print(f"Wrong!Its {result}. Better luck next time.")
+
+
+
+
+# import random
+# print("Rock , Paper , Scissor Game!!!")
+# print("Make your choice: ")
+# print(" Rock = 1 ")
+# print(" Paper = 2 ")
+# print(" Scissor = 3 ")
+# myChoice = int(input("Make your choice - (1 / 2 / 3 ) :  " ))
+# computerChoice = random.randint(1,3)
+# print(f"Computer choice: {computerChoice}")
+
+# if myChoice == computerChoice:
+#     print("Draw.")
+# elif myChoice == 1:  #Rock
+#     if computerChoice == 2:  #Paper
+#         print("You Lose, Computer Won!")
+#     else:
+#         print("You won!")
+
+# elif myChoice == 2:  #Paper
+#     if computerChoice == 3:  #Scissor
+#         print("You Lose, Computer Won!")
+#     else:
+#         print("You won!")
+
+# elif myChoice == 3:  #Scissor
+#     if computerChoice == 1:  #Rock
+#         print("You Lose, Computer Won!")
+#     else:
+#         print("You won!")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 6.2.Project6.py
@@ -0,0 +1,19 @@
+# import random
+# theList = ['father', 'enterprise', 'science', 'programming',
+#            'resistance', 'fiction', 'condition', 'reverse', 'computer', 'python']
+# computerChoice = random.choice(theList)
+# clue = computerChoice[0]
+# print(f"Clue - The first letter of the word will be: {clue}")
+
+# print("Guess the jumbled word: ")
+# print(' '.join(random.sample(computerChoice, len(computerChoice))))
+
+# myGuess = (str(input("The word is : ")))
+
+
+# if myGuess == computerChoice:
+#     print("Your guess is right..Congratulations!")
+# else:
+#     print("Oops! Your guessed wrong!")
+
+# print(f"The Word: {computerChoice}")
